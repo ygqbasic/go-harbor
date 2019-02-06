28bcddabba544c2607e2540511bb0d4c4345fff5
ygqbasic <ygqbasic@gmail.com>
2019-02-06 20:58:21 +0800

    Update harbor_test.go

--- harbor_test.go
+++ harbor_test.go
@@ -5,8 +5,6 @@ import (
 )
 
 func TestHarborStatistics(t *testing.T) {
-	t.Log("hello world")
-
 	harborClient := NewClient(nil, "https://reg.testcloud.com", "admin", "Harbor12345")
 	statisticMap, _, errs := harborClient.GetStatistics()
 	if errs == nil {
